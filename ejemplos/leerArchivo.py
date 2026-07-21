1df93653260a82eba9f9d0b25fae9f603a13fddc
Nahuel Paez <133921604+Franmarr@users.noreply.github.com>
2026-07-21 12:15:14 -0300

    Primera version de bloqueos, para un solo archivo
    Por el momento solo crea el json a partir del archivo

--- ejemplos/leerArchivo.py
+++ ejemplos/leerArchivo.py
@@ -1,18 +1,62 @@
+from decimal import Decimal
 import json
 from pathlib import Path
 
-CAMPOS = [(0,10,'padron'), (10,30,'nombre'), (30,40,'fecha'), (40,48,'sueldo'), (48,51,'pais')]
 registros = []
 
-with open(Path('dirA/titulares.txt'), 'r', encoding='utf-8') as f:
+def formatear_importe(valor: str) -> str:
+    importe = Decimal(valor) / Decimal('100')
+    parte_entera, parte_decimal = f"{importe:.2f}".split('.')
+    parte_entera_formateada = f"{int(parte_entera):,}".replace(',', '.')
+    return f"{parte_entera_formateada},{parte_decimal}"
+
+
+def formatear_cuil(cuil: str) -> str:
+    return f"{cuil[:2]}-{cuil[2:10]}-{cuil[10]}"
+
+with open(Path('fuentes/BSJ - ESCU0626/BSJ - ESCU/SUAC1.JUNIO26'), 'r', encoding='utf-8') as f:
     for linea in f:
         linea = linea.rstrip('\n\r')
+        #campo: fecha_pago
+        año= linea[6:8].strip()
+        mes= linea[8:10].strip()
+        dia= linea[10:12].strip()
+        #campo: cuenta_acreditacion_digito
+        numCuentaAcreditacion= linea[15:22]
+        digitoVerificador=linea[22]
+        #campo: tipoynumdoc
+        tipo=linea[68]
+        dni=linea[69:77]
+        #campo: sucursal_cuenta_digitodebito
+        sucursal=linea[77:79]
+        cuenta=linea[80:87]
+        digitoDebito=linea[79]
+        #campo: centro_sector
+        centro = linea[100:102]
+        sector= linea[102:105]
+        #campo: padron_digitoverificador
+        padron=linea[105:111]
+        digitoPadron=linea[111]
+
         registro = {
-            'padron': int(linea[0:10].strip() or 0),
-            'nombre': linea[10:30].strip(),
-            'fecha': linea[30:40].strip(),
-            'sueldo': float(linea[40:48].strip() or 0.0),
-            'pais': linea[48:51].strip()
+            'tipo_reg': linea[0].strip(),
+            'codigo_liquidacion': linea[1:3].strip(),
+            'centro_pago': linea[3:6].strip(),
+            'fecha_pago': f"{año}/{mes}/{dia}",
+            'sucursal_acreditacion': linea[12:14],
+            'tipo_acreditacion': linea[14],
+            'cuenta_acreditacion_digito': f"{numCuentaAcreditacion}/{digitoVerificador}", #consultar
+            'importe_acreditar':formatear_importe(float(linea[23:38])), #verificar
+            'ayn': (linea[38:68]).strip(),
+            'tipoynumdoc': f"{tipo}/{dni}",
+            'sucursal_cuenta_digitodebito': f"{sucursal}/{cuenta}/{digitoDebito}",
+            'cuil': formatear_cuil(linea[88:99]),
+            'zona': linea[99],
+            'centro_sector': f"{centro}/{sector}",
+            'padron_digitoverificador': f"{padron}/{digitoPadron}",
+            'codigo_banco': linea[126:128]
+
+
         }
         registros.append(registro)
 
@@ -20,3 +64,5 @@ with open('titulares.json', 'w', encoding='utf-8') as f:
     json.dump(registros, f, ensure_ascii=False, indent=2)
 
 print(f"✅ {len(registros)} registros guardados en titulares.json")
+print("-----")
+print(registros[0])
