9b6948f5ff4d1c0e95b2e15bf614dfbce148204b
Nahuel Paez <133921604+Franmarr@users.noreply.github.com>
2026-08-20 12:21:27 -0300

    Agregados mas filtros, y quitado boton activo de modal editar escuela

--- front/src/components/Escuelas/FormularioEditar.js
+++ front/src/components/Escuelas/FormularioEditar.js
@@ -31,5 +31,5 @@ export const formularioCampos = [
   {name: "motivo", label: "Motivo", placeholder: "Motivo", type: "select", optionsKey: "motivos"},
   {name: "login", label: "Login", placeholder: "no_visible", type: "text"},
   {name: "empresa", label: "Empresa", placeholder: "no_visible", type: "text"},
-  {name: "activo", label: "Activo", placeholder: "Activo", type: "checkbox"},
+  {name: "activo", label: "Activo", placeholder: "no_visible", type: "checkbox"},
 ];
